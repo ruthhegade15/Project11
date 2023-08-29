aad43df5315f9440b410c3e11f3f6bb67a4290ca
ruthhegade15 <107542610+ruthhegade15@users.noreply.github.com>
2023-08-30 01:42:53 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Emailer from './Components/Emailer';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div>
+      <Emailer/>
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Emailer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Emailer() {
+  return (
+    <>
+      <div>
+        Hello i am Emailer
+      </div>
+    </>
+  )
+}
+
+export default Emailer
